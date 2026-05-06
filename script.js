ba1c09270f74d898ad16cc9b137e094c6dd52adf
MaxiPineyrua <111310426+MaxiPineyrua@users.noreply.github.com>
2026-05-06 17:38:46 -0300

    JavaScript completado (¿a pulir?) + Creación del CSS

---
+++ script.js
@@ -0,0 +1,83 @@
+let tareas = [];
+
+const tareasGuardadas = localStorage.getItem("tareas");
+if (tareasGuardadas) {
+  tareas = JSON.parse(tareasGuardadas);
+}
+
+//Seleccionemos los elementos
+const input = document.querySelector("#inputTarea")
+const boton = document.querySelector("#btnAgregar")
+const lista = document.querySelector("#listaTareas")
+
+//Mostrar tareas guardadas
+tareas.forEach(function(tarea, index){
+  //Crea el <li>
+  const li = document.createElement("li");
+  //Contenido del <li>
+  li.textContent = tarea + "    ";
+  li.addEventListener("click", function(){
+    li.classList.toggle("completada");
+  })
+
+  const btnEliminar =document.createElement("button");
+  btnEliminar.textContent = "Eliminar";
+
+  //Evento para eliminar
+  btnEliminar.addEventListener("click", function() {
+    li.remove();
+    tareas.splice(index, 1);
+    localStorage.setItem("tareas", JSON.stringify(tareas));  
+  });
+
+  //Agregar botón de eliminar al li
+  li.appendChild(btnEliminar);
+
+  //Se agrega a la lista
+  lista.appendChild(li);
+  
+})
+
+//Al hacer click al botón
+boton.addEventListener("click", function() {
+  //Texto del input
+  const texto = input.value;
+  if (texto === "") return;
+  //Revisamos si la tarea ya existe
+  if(tareas.some(tarea => tarea.toLowerCase() === texto.toLowerCase())){
+    alert("Esa tarea ya existe! ");
+    return;
+  }
+
+  //Agregamos el texto a la lista
+  const index = tareas.length
+  tareas.push(texto);
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+  //Crea el <li>
+  const li = document.createElement("li");
+  //Contenido del <li>
+  li.textContent = texto + "    ";
+  li.addEventListener("click", function(){
+    li.classList.toggle("completada");
+  })
+
+  //Boton para eliminar
+  const btnEliminar = document.createElement("button");
+  btnEliminar.textContent = "Eliminar";
+
+  //Evento para eliminar una tarea
+  btnEliminar.addEventListener("click", function() {
+    li.remove();
+    tareas.splice(index, 1);
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+  });
+
+  //Agregar botón de eliminar al li
+  li.appendChild(btnEliminar);
+
+  //Se agrega a la lista
+  lista.appendChild(li);
+
+  //Vaciar el input
+  input.value = "";
+})
